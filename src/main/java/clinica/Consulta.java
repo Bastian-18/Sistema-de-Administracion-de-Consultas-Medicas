b951256f29351dc72be47b270318e31315384813
Renesls <renesl@uamv.edu.ni>
2025-11-12 16:36:51 -0600

    Entidades con lombok

--- src/main/java/clinica/Consulta.java
+++ src/main/java/clinica/Consulta.java
@@ -1,5 +1,10 @@
 package clinica;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Consulta {
     
     String fecha;
@@ -33,60 +38,5 @@ public class Consulta {
                 '}';
     }
 
-    public String getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
-
-    public String getHora() {
-        return hora;
-    }
-
-    public void setHora(String hora) {
-        this.hora = hora;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Doctor getDoctor() {
-        return doctor;
-    }
-
-    public void setDoctor(Doctor doctor) {
-        this.doctor = doctor;
-    }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getTotalCosto() {
-        return TotalCosto;
-    }
-
-    public void setTotalCosto(String totalCosto) {
-        TotalCosto = totalCosto;
-    }
 }
 
--- src/main/java/clinica/SistemaClinico.java
+++ src/main/java/clinica/SistemaClinico.java
@@ -1,37 +1,18 @@
 package clinica;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@Getter
 public class SistemaClinico {
     
     List<Cliente> clientes;
     List<Doctor> doctores;
     List<Consulta> consultas;
-
-    public List<Cliente> getClientes() {
-        return clientes;
-    }
-
-    public void setClientes(List<Cliente> clientes) {
-        this.clientes = clientes;
-    }
-
-    public List<Doctor> getDoctores() {
-        return doctores;
-    }
-
-    public void setDoctores(List<Doctor> doctores) {
-        this.doctores = doctores;
-    }
-
-    public List<Consulta> getConsultas() {
-        return consultas;
-    }
-
-    public void setConsultas(List<Consulta> consultas) {
-        this.consultas = consultas;
-    }
 
     @Override
     public String toString() {
